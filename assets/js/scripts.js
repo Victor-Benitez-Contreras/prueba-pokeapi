f6a353ff327e424e9f08e9d35698ff4180634637
Victor Benitez <neoxkrl@gmail.com>
2020-02-25 17:22:41 -0300

    commit sala

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
 
 $('#pokesearch').click(function() {
-
-
-$.ajax({
+  // Rescatando el valor ingresado
+  var pokemonName = $('#pokemon-name').val();
+  var pokemonNameLower = pokemonName.toLowerCase();
+  $.ajax({
     url: `https://pokeapi.co/api/v2/pokemon/${pokemonNameLower}`,
     type: 'GET',
     datatype: 'json',
@@ -10,4 +11,20 @@ $.ajax({
       console.log(results);
     }
   }).done(response).fail(error);
+  
+  function response(data) {
+    $('#pokemon-container').empty();
+    const pakimonImg = data.sprites.front_default;
+    const pakimonName = data.name;
+    const pakimonNumber = data.order;
+    const pakimonWeight = data.weight;
+    const pakimonAbbilities = data.abilities[0].ability.name;
+    $('.info-modal').empty();
+    $('#pokemon-container').append(`<img class="pakimon-imagen pakimon-img-modal" src="${pakimonImg}" data-toggle="modal" data-target="#myModal"><h3><strong>${data.name}</strong></h3>`);
+    $('.info-modal').append(`<p>Peso: ${pakimonWeight}</p><p>Número: ${pakimonNumber}<p>Habilidades: ${pakimonAbbilities}</p>`);
+  }
 
+  function error() {
+    alert('Lo sentimos, ha ocurrido un error :(');
+  }
+});
